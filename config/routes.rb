e5e427cb0a091505f97afe30f85768a0f83b3191
kegbenk <kegbenk@me.com>
2015-04-29 15:54:27 -0500

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
   get 'static_pages/help'
 
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#hello'
+  #root 'application#hello'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
